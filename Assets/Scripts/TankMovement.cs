0bf97991a5cea57f81e320d5104c9a704ccae57a
Cookie147 <57656254+Cookie147@users.noreply.github.com>
2019-11-12 12:00:35 +0100

    Initial commit

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        //Tank movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(transform.right * -moveHorizontal * Time.deltaTime * speed);
+        transform.Translate(transform.forward * moveVertical * Time.deltaTime * speed);
+
+        //Tank rotation
+        Vector3 moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, newRotation, Time.deltaTime * 8);
+        }
+    }
+}
