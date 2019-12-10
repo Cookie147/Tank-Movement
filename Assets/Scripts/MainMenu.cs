9029ec9d2c1afbdc1b8b67f7c2a9abed9e747ac5
Cookie147 <57656254+Cookie147@users.noreply.github.com>
2019-12-10 16:02:11 +0100

    Menu with Submenus

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject playerTank;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        playerTank = GameObject.Find("Player Tank");
+        //change color of tank
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
