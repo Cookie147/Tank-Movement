8c259560a98e4f45a5774450de60e67f09458ab4
Cookie147 <57656254+Cookie147@users.noreply.github.com>
2019-12-14 00:36:33 +0100

    Level 2 in progress

--- Assets/Scripts/Storage.cs
+++ Assets/Scripts/Storage.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Storage : MonoBehaviour
 {
     public int maxShots;
-
+    //Sounds
 
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
         maxShots = 5;
     }
-    
 
+    //Difficulty Settings
     public void setNumShots(int numShots)
     {
         maxShots = numShots;
