9e9fdbd0537602cffb855f0be6720de1c13761b5
Cookie147 <57656254+Cookie147@users.noreply.github.com>
2019-12-13 20:36:56 +0100

    SetOnFire in progress

--- Assets/Scripts/Hay.cs
+++ Assets/Scripts/Hay.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class Hay : MonoBehaviour
 {
-    public void SetOnFire()
+    public GameObject firePrefab;
+    private ParticleSystem fireParticles;
+
+
+    void Start()
     {
 
+        //get all animation components
+        fireParticles = Instantiate(firePrefab).GetComponent<ParticleSystem>();
+
+        fireParticles.gameObject.SetActive(false);
+    }
+
+    public void SetOnFire()
+    {
+        fireParticles.transform.position = transform.position;
+        fireParticles.gameObject.SetActive(true);
+
+        fireParticles.Play();
+
+        //tankExplosionAudio.Play();
+
+        Destroy(fireParticles);
+        Destroy(gameObject);
     }
 }
